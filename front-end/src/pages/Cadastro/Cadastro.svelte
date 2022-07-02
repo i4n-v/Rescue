0e3e4b0a13c132a066a7338ecbcb86bde7fc05d2
Ian Vinícius <ianvinicioss@gmail.com>
2022-07-02 12:42:36 -0300

    Componentes de Formulário e Valudação (#4)
    * Deletando svg
    
    * Adicionando link de dúvidas
    
    * Adicionando componente de input de formulário e funções de validação
    
    * Criando botão de envio de formulário
    
    * Mudando limite de caracteres para cpf
    
    * Adicionando botão de envio na tela de cadastro

--- front-end/src/pages/Cadastro/Cadastro.svelte
+++ front-end/src/pages/Cadastro/Cadastro.svelte
@@ -1,23 +1,20 @@
 <!-- código JavaScript -->
 <script>
-
-import { Link } from "../../components/";
-import Button from "../../components/Buttons/OutlinedButton.svelte";
-
+  import { Link, SubmitButton } from "../../components/";
 
   let user = {
-    nome: '',
-    cpf: '',
-    email: '',
-    pwd: ''
+    nome: "",
+    cpf: "",
+    email: "",
+    pwd: "",
   };
-  let confPwd = '';
+  let confPwd = "";
 
   let erros = {
-    nome: '',
-    cpf: '',
-    email: '',
-    pwd: ''
+    nome: "",
+    cpf: "",
+    email: "",
+    pwd: "",
   };
   let validar = false;
 
@@ -26,41 +23,38 @@ import Button from "../../components/Buttons/OutlinedButton.svelte";
     // nome
     if (user.nome.trim().length < 1) {
       validar = false;
-      erros.nome = 'Insira seu nome.'
+      erros.nome = "Insira seu nome.";
     } else {
-      erros.nome = ''
+      erros.nome = "";
     }
     // cpf
     if (user.cpf.trim().length < 1) {
       validar = false;
-      erros.cpf = 'Insira seu CPF.'
+      erros.cpf = "Insira seu CPF.";
     } else {
-      erros.cpf = ''
+      erros.cpf = "";
     }
     // email
     if (user.email.trim().length < 1) {
       validar = false;
-      erros.email = 'Insira seu Email.'
+      erros.email = "Insira seu Email.";
     } else {
-      erros.email = ''
+      erros.email = "";
     }
     // pwd
     if (user.pwd.trim().length < 1) {
       validar = false;
-      erros.pwd = 'Insira sua senha.'
+      erros.pwd = "Insira sua senha.";
     } else {
-      erros.pwd = ''
+      erros.pwd = "";
     }
   }
-  
-
 </script>
-
 
 <!-- código HTML -->
 <main>
   <div class="img-container">
-    <img src="/src/assets/img/aside-logo.png" alt="Logo da Rescue">
+    <img src="/src/assets/img/aside-logo.png" alt="Logo da Rescue" />
   </div>
 
   <div class="form-container">
@@ -69,44 +63,62 @@ import Button from "../../components/Buttons/OutlinedButton.svelte";
     <form>
       <div class="form-field">
         <label for="nome">Nome</label>
-        <input type="text" id="nome" placeholder="Seu nome" bind:value={user.nome}>
-        <div class="error">{ erros.nome }</div>
+        <input
+          type="text"
+          id="nome"
+          placeholder="Seu nome"
+          bind:value={user.nome}
+        />
+        <div class="error">{erros.nome}</div>
       </div>
       <div class="form-field">
         <label for="cpf">CPF</label>
-        <input type="text" id="cpf" placeholder="Insira somente números" bind:value={user.cpf}>
-        <div class="error">{ erros.cpf }</div>
+        <input
+          type="text"
+          id="cpf"
+          placeholder="Insira somente números"
+          bind:value={user.cpf}
+        />
+        <div class="error">{erros.cpf}</div>
       </div>
       <div class="form-field">
         <label for="email">Email</label>
-        <input type="email" id="email" placeholder="contato@email.com" bind:value={user.email}>
-        <div class="error">{ erros.email }</div>
+        <input
+          type="email"
+          id="email"
+          placeholder="contato@email.com"
+          bind:value={user.email}
+        />
+        <div class="error">{erros.email}</div>
       </div>
       <div class="form-field">
         <label for="senha">Senha</label>
-        <input type="password" id="senha" bind:value={user.pwd}>
-        <div class="error">{ erros.pwd }</div>
+        <input type="password" id="senha" bind:value={user.pwd} />
+        <div class="error">{erros.pwd}</div>
       </div>
       <div class="form-field">
         <label for="confSenha">Confirmar senha</label>
-        <input type="password" id="confSenha" bind:value={confPwd}>
-        <div class="error">{ erros.pwd }</div>
+        <input type="password" id="confSenha" bind:value={confPwd} />
+        <div class="error">{erros.pwd}</div>
       </div>
-      <p>Já possui uma conta? Então <Link href="/login">interaja conosco</Link>.</p>
-    <div class="botao" on:click={submit}>
-      <Button>REGISTRAR-SE</Button>
-    </div>
+      <p>
+        Já possui uma conta? Então <Link href="/login">interaja conosco</Link>.
+      </p>
+      <div class="botao" on:click={submit}>
+        <SubmitButton
+          onSubmit={function () {
+            console.log("Enviou");
+          }}
+        >
+          REGISTRAR-SE
+        </SubmitButton>
+      </div>
     </form>
-
   </div>
-
 </main>
-
-
 
 <!-- código CSS -->
 <style>
-
   main {
     display: flex;
   }
@@ -117,13 +129,13 @@ import Button from "../../components/Buttons/OutlinedButton.svelte";
     font: var(--poppins-xxl);
   }
   .dot {
-    color: #4892BC;
+    color: #4892bc;
   }
   .botao {
     margin-top: 2rem;
   }
-  .error{
+  .error {
     font: var(--poppins-xs);
-    color: #C65454;
+    color: #c65454;
   }
 </style>
