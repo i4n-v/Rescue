f9d1459b570bed79d23b69d89f0060847e11c1d4
i4n-v <ianvinicioss@gmail.com>
2022-07-02 12:38:17 -0300

    Adicionando componente de input de formulário e funções de validação

---
+++ front-end/src/components/FormInput/FormInput.svelte
@@ -0,0 +1,86 @@
+<script>
+  // Atributes
+  export let id = "";
+  export let type = "text";
+  export let placeholder = "";
+  export let className = "";
+  export let disabled = false;
+  export let required = false;
+  export let maxlength = false;
+  export let label = "Label";
+  export let value = null;
+  export let error = null;
+
+  // Events
+  export let setValue = () => {};
+</script>
+
+<div class="field-container">
+  <label class="label-form" for={id}>
+    {label}
+    <span>{required ? "*" : ""}</span>
+  </label>
+  <input
+    {id}
+    name={id}
+    {type}
+    {placeholder}
+    {value}
+    {disabled}
+    {maxlength}
+    class={`field-form ${className}`}
+    on:keypress={({ target }) => setValue(target.value)}
+    on:change={({ target }) => setValue(target.value)}
+    on:blur={({ target }) => setValue(target.value)}
+  />
+  {#if !!error}
+    <p>{error}</p>
+  {/if}
+</div>
+
+<style>
+  .field-container {
+    display: flex;
+    flex-direction: column;
+    gap: 4px;
+  }
+
+  .field-form {
+    min-width: 250px;
+    min-height: 48px;
+    padding: 0px 10px;
+    background: var(--c01);
+    border: 1px solid var(--c02);
+    border-radius: 5px;
+    font: var(--roboto-s);
+    color: var(--c07);
+    transition: 0.3s;
+  }
+
+  .firld-form::placeholder {
+    font: var(--roboto-s);
+    color: var(--c07);
+  }
+
+  .field-form:focus,
+  .field-form:hover {
+    outline: none;
+    border-color: var(--p02);
+    box-shadow: 0px 0px 0px 2px var(--p01);
+  }
+
+  .label-form {
+    font: var(--poppins-s);
+    font-weight: 500;
+    color: var(--c11);
+  }
+
+  .label-form > span {
+    color: var(--v02);
+  }
+
+  .field-container > p {
+    font: var(--roboto-xs);
+    color: var(--v03);
+  }
+</style>
