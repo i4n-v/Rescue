2583571b80e44123af5c2edfc1919c891cd53ad5
Andrey Mafra <102834124+andreymafra55@users.noreply.github.com>
2022-07-03 15:27:02 -0300

    criacao do html e parte do estilo

---
+++ front-end/src/pages/Duvidas/Duvidas.svelte
@@ -0,0 +1,68 @@
+<script>
+
+</script>
+
+<div class="box">
+<h1>Perguntas Frequentes</h1>
+    <ul class="list">
+        <li class="list-item-grey">
+            <div class="list-quest" onclick="show(0);">
+                <p>Como vocês nos ajudam ?</p>
+            </div>
+            <div class="list-answer">
+                <p>Ajudamos a comunidade através da visibilidade dos problemas expostos pelos usuários, onde as ongs e instituições cadastradas na plataforma tentarão solucionar a problemática.</p>
+            </div>
+        </li>
+        <li class="list-item-white">
+            <div class="list-quest">
+                <p>Como posso entrar em contato?</p>
+            </div>
+            <div class="list-answer">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est ullam, ab eveniet dicta quaerat temporibus impedit nobis, tenetur et excepturi voluptatum. Voluptatem fuga atque veritatis nobis magni odio accusamus sint!</p>
+            </div>
+        </li>
+        <li class="list-item-grey">
+            <div class="list-quest" onclick="show(0);">
+                <p>O que acontece com meus dados?</p>
+            </div>
+            <div class="list-answer">
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae laborum alias doloribus velit nulla, quod aliquid vitae quis aliquam suscipit, odit quibusdam natus amet laudantium molestiae? A eaque aliquid quae.</p>
+            </div>
+        </li>
+        <li class="list-item-white">
+            <div class="list-quest" onclick="show(0);">
+                <p>Para que servem minhas postagens?</p>
+            </div>
+            <div class="list-answer">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam ea numquam iure. Debitis ad provident cupiditate minima tenetur expedita non necessitatibus deleniti. Tempora culpa placeat in suscipit obcaecati, molestias dolore?.</p>
+            </div>
+        </li>
+        <li class="list-item-grey">
+            <div class="list-quest" onclick="show(0);">
+                <p>O que as instituições fazem?</p>
+            </div>
+            <div class="list-answer">
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur magnam nesciunt atque, eligendi iusto porro ea vel. Est itaque saepe deserunt vitae facilis soluta. Blanditiis, asperiores ea. Voluptatibus, magnam rem?</p>
+            </div>
+        </li>
+    </ul>
+</div>
+
+<style>
+h1{
+    color: var(--c11);
+    font-family: var(--poppins-xxl);
+    font-weight: bold;
+}
+
+.list-quest{
+    color: var(--c11);
+    font-family: var(--poppins-m);
+}
+
+.list-answer{
+    color: var(--c09);
+    font-family:var(----roboto-m);
+}
+</style>
+
