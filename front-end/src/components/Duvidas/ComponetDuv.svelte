b57e64076958987dd07790c3a89ac35f8a2a40a2
Andrey Mafra <102834124+andreymafra55@users.noreply.github.com>
2022-07-04 01:42:19 -0300

    cricao do componente

---
+++ front-end/src/components/Duvidas/ComponetDuv.svelte
@@ -0,0 +1,91 @@
+<script>
+import BluePoint from  "../../assets/svg/BluePoint.svelte";
+import Dropdown from  "../../assets/svg/Dropdown.svelte";
+export let pergunta = "";
+export let resposta = "";
+
+let display = false;
+function isRotated(){
+    display = !display
+}
+function click(){
+    display = !display
+}
+</script>
+    <ul class="list">
+        <li class="list-item">
+            
+            <div class="list-quest" >
+                <span class="point">
+                    <BluePoint/>
+                </span>
+                <h2>
+                    {pergunta}
+                </h2>
+                <span class={`dropdown ${display ? "rotate" : ""}`}  on:click={click}  >
+                    <Dropdown/>
+                </span>
+            </div>
+            <div class={display ? "list-answer-off" : "list-answer-on"} >
+                <p>{resposta}</p>
+            </div>
+        </li>
+    </ul>
+<style>
+.list{
+    padding: 20px;
+    list-style: none;
+    position: relative;
+}
+
+.list-item{
+    padding:20px;
+}
+
+.list-quest{
+    color: var(--c11);
+    font: var(--poppins-m);
+    margin-bottom:15px;
+    display: flex;
+    align-items:center ;
+    gap: 8px;
+}
+.list-answer-on{
+    color: var(--c09);
+    font:var(--roboto-m);
+    display: block;
+}
+.list-answer-off{
+    color: var(--c09);
+    font:var(--roboto-m);
+    display: none;
+}
+
+ .list-item .dropdown{
+    position: absolute;
+    right: 12px;
+    cursor: pointer;
+}
+
+:global(.rotate){
+    animation: rotate 0.3s;
+}
+
+@media(max-width : 600px){
+    .list-quest{
+        font: var(--poppins-s);
+    }
+    .list-answer-on{
+    font: var(--roboto-s);
+    }
+}
+
+@media(max-width :400px){
+    .list-quest{
+        font: var(--poppins-xs);
+    }
+    .list-answer-on{
+    font: var(--roboto-xs);
+    }
+}
+</style>
