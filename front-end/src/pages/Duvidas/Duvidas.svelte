ba924d823a06780207afb7a182d212dbd2f6d073
Andrey Mafra <102834124+andreymafra55@users.noreply.github.com>
2022-07-04 01:43:58 -0300

    Utilizando componente na pagina do app(Todas utilidades funcionando)

--- front-end/src/pages/Duvidas/Duvidas.svelte
+++ front-end/src/pages/Duvidas/Duvidas.svelte
@@ -1,68 +1,52 @@
 <script>
-
+ import {ComponetDuv} from "../../components";
 </script>
 
 <div class="box">
-<h1>Perguntas Frequentes</h1>
-    <ul class="list">
-        <li class="list-item-grey">
-            <div class="list-quest" onclick="show(0);">
-                <p>Como vocês nos ajudam ?</p>
-            </div>
-            <div class="list-answer">
-                <p>Ajudamos a comunidade através da visibilidade dos problemas expostos pelos usuários, onde as ongs e instituições cadastradas na plataforma tentarão solucionar a problemática.</p>
-            </div>
-        </li>
-        <li class="list-item-white">
-            <div class="list-quest">
-                <p>Como posso entrar em contato?</p>
-            </div>
-            <div class="list-answer">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est ullam, ab eveniet dicta quaerat temporibus impedit nobis, tenetur et excepturi voluptatum. Voluptatem fuga atque veritatis nobis magni odio accusamus sint!</p>
-            </div>
-        </li>
-        <li class="list-item-grey">
-            <div class="list-quest" onclick="show(0);">
-                <p>O que acontece com meus dados?</p>
-            </div>
-            <div class="list-answer">
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae laborum alias doloribus velit nulla, quod aliquid vitae quis aliquam suscipit, odit quibusdam natus amet laudantium molestiae? A eaque aliquid quae.</p>
-            </div>
-        </li>
-        <li class="list-item-white">
-            <div class="list-quest" onclick="show(0);">
-                <p>Para que servem minhas postagens?</p>
-            </div>
-            <div class="list-answer">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam ea numquam iure. Debitis ad provident cupiditate minima tenetur expedita non necessitatibus deleniti. Tempora culpa placeat in suscipit obcaecati, molestias dolore?.</p>
-            </div>
-        </li>
-        <li class="list-item-grey">
-            <div class="list-quest" onclick="show(0);">
-                <p>O que as instituições fazem?</p>
-            </div>
-            <div class="list-answer">
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur magnam nesciunt atque, eligendi iusto porro ea vel. Est itaque saepe deserunt vitae facilis soluta. Blanditiis, asperiores ea. Voluptatibus, magnam rem?</p>
-            </div>
-        </li>
-    </ul>
+    <h1 id="titulo"> Perguntas Frequentes<span>.</span></h1>
+     <div class="list-item-grey">
+        <ComponetDuv pergunta="Como vocês nos ajudam ?" resposta="Ajudamos a comunidade através da visibilidade dos problemas expostos pelos usuários, onde as ongs e instituições cadastradas na plataforma tentarão solucionar a problemática."/>
+    </div> 
+    <div>
+        <ComponetDuv pergunta="O que acontece com meus dados ?" resposta="Todos os seus dados são protegidos de acordo com a Lei Geral de Proteção de Dados Pessoais( LGPD). Garantindo privacidade e sigilo das informações."/>
+    </div>
+    <div class="list-item-grey">
+        <ComponetDuv pergunta="Como vocês nos ajudam ?" resposta="Ajudamos a comunidade através da visibilidade dos problemas expostos pelos usuários, onde as ongs e instituições cadastradas na plataforma tentarão solucionar a problemática."/>
+    </div>
+    <div>
+        <ComponetDuv pergunta="Para que servem minhas postagens ?" resposta="Suas postagens permitirão que instituições, e outros usuários, possam tomar conhecimento de problemas na região. "/>
+    </div>
+    <div class="list-item-grey">
+        <ComponetDuv pergunta="O que as instituições fazem ?" resposta="Depois de identificar e validar a problemática, as instituições auxiliam na tentativa de minimizar os prejuízos causados.  "/>
+    </div>
 </div>
 
 <style>
-h1{
+#titulo{
     color: var(--c11);
-    font-family: var(--poppins-xxl);
-    font-weight: bold;
+    font: var(--poppins-xxl);
+    margin-bottom:30px;
+}
+.box{
+    margin: 120px ;
 }
 
-.list-quest{
-    color: var(--c11);
-    font-family: var(--poppins-m);
+.list-item-grey{
+    background-color: var(--c01);
 }
 
-.list-answer{
-    color: var(--c09);
-    font-family:var(----roboto-m);
+span {
+    color: var(--p01);
+}
+
+@media(max-width:600px){
+    .box{margin: 70px;}
+    #titulo{
+        font:var(--poppins-xl)
+    }
+}
+@media(max-width:400px){
+   
+    .box{margin: 50px;}
 }
 </style>
-
