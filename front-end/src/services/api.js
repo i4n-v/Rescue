9e1d6b9c50581a8757151806efda47c7109d234a
i4n-v <ianvinicioss@gmail.com>
2022-07-05 23:16:19 -0300

    Criando servços iniciais do usuário e trabalhando com cookies

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -5,7 +5,7 @@ const instance = axios.create({
   baseURL: "http://127.0.0.1:8000/api"
 });
 
-const token = cookie.get("acess-token");
+const token = cookie.get("access-token");
 
 if (token) {
   instance.defaults.headers.common = { 'Authorization': `Bearer ${token}` }
