30aaab8af9513130e6fb3f764e2b70f23d7dffa3
i4n-v <ianvinicioss@gmail.com>
2022-07-05 22:19:59 -0300

    Finalizando tela de cadastro e integração com api

---
+++ front-end/src/services/api.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+import cookie from "js-cookie";
+
+const instance = axios.create({
+  baseURL: "http://127.0.0.1:8000/api"
+});
+
+const token = cookie.get("acess-token");
+
+if (token) {
+  instance.defaults.headers.common = { 'Authorization': `Bearer ${token}` }
+}
+
+export default instance;
