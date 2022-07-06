689f9852d019a50230806ef157b86c596a4fe71f
Ian Vinícius <ianvinicioss@gmail.com>
2022-07-05 23:23:42 -0300

    Ratas de cadastro e autenticação (#9)
    * Configurando rotas e a api
    
    * Adicionando coluna para o token de autenticação na tabela usuário e configurando conexão mysql
    
    * Criando rota de cadastro de pessoas
    
    * Criando rota de cadastro de instituições
    
    * Criando rota de autenticação de usuários
    
    * Adicionada rota de consulta de usuário utilizando token
    
    * Criando o endereço juntamente com o usuário e retornando na hora da busca
    
    * Corrigindo erro de cors e adicionando permissão da aplicação em svelte na api

--- api/index.php
+++ api/index.php
@@ -1,14 +1,22 @@
 <?php
+header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
 require_once "./src/dataBase/conectDatabase.php";
 
 $requestMethod = $_SERVER["REQUEST_METHOD"];
 $request = $_SERVER['REQUEST_URI'];
 
-if ($request == "/") {
+if ($request == "/api") {
   echo "Hello World";
+} else   if ($requestMethod == "OPTIONS") {
+  http_response_code(200);
+  echo "OK";
 } else if ($requestMethod == "GET") {
   switch ($request) {
-    case str_contains($request, '/user'):
+    case str_contains($request, '/api/user'):
       require __DIR__ . '/src/controllers/UserController/getUser.php';
       break;
     default:
@@ -18,13 +26,13 @@ if ($request == "/") {
   }
 } else if ($requestMethod == "POST") {
   switch ($request) {
-    case '/people':
+    case '/api/people':
       require __DIR__ . '/src/controllers/PeopleController/createPeople.php';
       break;
-    case '/institution':
+    case '/api/institution':
       require __DIR__ . '/src/controllers/InstitutionController/createInstitution.php';
       break;
-    case '/login':
+    case '/api/login':
       require __DIR__ . '/src/controllers/UserController/login.php';
       break;
     default:
