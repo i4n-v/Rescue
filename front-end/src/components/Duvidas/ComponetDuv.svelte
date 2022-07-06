5e36efcb1e911e3044b315fdc27924856c4fb8dc
i4n-v <ianvinicioss@gmail.com>
2022-07-05 22:14:48 -0300

    Retirando scale animation da página de dúvidas

--- front-end/src/components/Duvidas/ComponetDuv.svelte
+++ front-end/src/components/Duvidas/ComponetDuv.svelte
@@ -1,98 +1,96 @@
 <script>
-import BluePoint from  "../../assets/svg/BluePoint.svelte";
-import Dropdown from  "../../assets/svg/Dropdown.svelte";
-import { onMount } from "svelte";
-export let pergunta = "";
-export let resposta = "";
+  import BluePoint from "../../assets/svg/BluePoint.svelte";
+  import Dropdown from "../../assets/svg/Dropdown.svelte";
+  export let pergunta = "";
+  export let resposta = "";
 
-let display = false;
+  let display = false;
 
-function click(){
-    display = !display
-}
-
-onMount(() => {
-    window.scrollTo(0, 0);
-  });
+  function click() {
+    display = !display;
+  }
 </script>
 
 <ul class="list">
-    <li class="list-item">
-        <div class="list-quest" >
-            <span class="point">
-                <BluePoint/>
-            </span>
-            <h2>
-                {pergunta}
-            </h2>
-            <span class={`dropdown ${display ? "rotate" : "rotate-reverse"}`}  on:click={click}  >
-                <Dropdown/>
-            </span>
-        </div>
-        <div class={display ? "list-answer-on" : "list-answer-off"} >
-            <p>{resposta}</p>
-        </div>
-    </li>
+  <li class="list-item">
+    <div class="list-quest">
+      <span class="point">
+        <BluePoint />
+      </span>
+      <h2>
+        {pergunta}
+      </h2>
+      <span
+        class={`dropdown ${display ? "rotate" : "rotate-reverse"}`}
+        on:click={click}
+      >
+        <Dropdown />
+      </span>
+    </div>
+    <div class={display ? "list-answer-on" : "list-answer-off"}>
+      <p>{resposta}</p>
+    </div>
+  </li>
 </ul>
+
 <style>
-.list{
+  .list {
     padding: 20px;
     list-style: none;
     position: relative;
-}
+  }
 
-.list-item{
-    padding:20px;
-}
+  .list-item {
+    padding: 20px;
+  }
 
-.list-quest{
+  .list-quest {
     color: var(--c11);
     font: var(--poppins-m);
-    margin-bottom:15px;
+    margin-bottom: 15px;
     display: flex;
-    align-items:center ;
+    align-items: center;
     gap: 8px;
-}
-.list-answer-on{
+  }
+  .list-answer-on {
     color: var(--c09);
-    font:var(--roboto-m);
+    font: var(--roboto-m);
     display: block;
-    animation: scale 0.3s ease-in ;
-}
-.list-answer-off{
+  }
+  .list-answer-off {
     color: var(--c09);
-    font:var(--roboto-m);
+    font: var(--roboto-m);
     display: none;
-}
+  }
 
- .list-item .dropdown{
+  .list-item .dropdown {
     position: absolute;
     right: 12px;
     cursor: pointer;
-}
+  }
 
-.rotate{
+  .rotate {
     animation: rotate 0.5s forwards;
-}
+  }
 
-.rotate-reverse{
+  .rotate-reverse {
     animation: rotate-reverse 0.5s forwards;
-}
-@media(max-width : 600px){
-    .list-quest{
-        font: var(--poppins-s);
+  }
+  @media (max-width: 600px) {
+    .list-quest {
+      font: var(--poppins-s);
     }
-    .list-answer-on{
-    font: var(--roboto-s);
+    .list-answer-on {
+      font: var(--roboto-s);
     }
-}
+  }
 
-@media(max-width :400px){
-    .list-quest{
-        font: var(--poppins-xs);
+  @media (max-width: 400px) {
+    .list-quest {
+      font: var(--poppins-xs);
     }
-    .list-answer-on{
-    font: var(--roboto-xs);
+    .list-answer-on {
+      font: var(--roboto-xs);
     }
-}
+  }
 </style>
