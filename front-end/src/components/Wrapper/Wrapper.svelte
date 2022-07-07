80a1a8f4fabaf18f4efb365ba4a9c3305ac22e7b
i4n-v <ianvinicioss@gmail.com>
2022-07-06 23:57:32 -0300

    Implementando autenticação baseada em token/cookies

--- front-end/src/components/Wrapper/Wrapper.svelte
+++ front-end/src/components/Wrapper/Wrapper.svelte
@@ -1,6 +1,17 @@
+<script>
+  import { Header, Footer } from "../";
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    window.scrollTo(0, 0);
+  });
+</script>
+
+<Header />
 <main id="wrapper">
   <slot />
 </main>
+<Footer />
 
 <style>
   #wrapper {
