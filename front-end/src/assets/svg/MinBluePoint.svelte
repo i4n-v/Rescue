28ea05ec950621fac853e126394b4fa0a6cbce06
i4n-v <ianvinicioss@gmail.com>
2022-07-12 23:18:59 -0300

    Adicionando novos ícones

---
+++ front-end/src/assets/svg/MinBluePoint.svelte
@@ -0,0 +1,9 @@
+<svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="4" cy="4" r="4" fill="url(#paint0_linear_141_559)"/>
+<defs>
+<linearGradient id="paint0_linear_141_559" x1="4" y1="0" x2="4" y2="8" gradientUnits="userSpaceOnUse">
+<stop stop-color="#4892BC"/>
+<stop offset="1" stop-color="#1F668D"/>
+</linearGradient>
+</defs>
+</svg>
